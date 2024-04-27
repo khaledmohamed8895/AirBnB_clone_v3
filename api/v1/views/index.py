479f9578eb0de12be94d73320dccf22d34ad3b1a
amr9 <amr12201148@yahoo.com>
2024-04-27 17:10:09 +0000

    redone

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,28 @@
 #!/usr/bin/python3
-"""
-index
-"""
-
-from flask import jsonify
+"""Module to define a flask blueprint"""
 from api.v1.views import app_views
-
+from flask import jsonify
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status():
-    """
-    status route
-    :return: response with json
-    """
-    data = {
-        "status": "OK"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    """return a json string"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+@app_views.route("/stats", strict_slashes=False)
 def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+    """retrieves the number of each objects by type"""
+    classes = {
+        "amenities": "Amenity",
+        "cities": 'City',
+        "places": 'Place',
+        "reviews": 'Review',
+        "states": 'State',
+        "users": 'User'
     }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    stats = {}
+    for key, value in classes.items():
+        stats[key] = storage.count(value)
+    return jsonify(stats)
