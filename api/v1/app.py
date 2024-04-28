d53d1c2710e4b301c89ea85fb8171f7ec0ea784c
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-04-28 20:54:10 +0300

    updating

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,46 @@
 #!/usr/bin/python3
-"""This module start Flask application"""
-from api.v1.views import app_views
+"""
+app
+"""
+
 from flask import Flask, jsonify
-from models import storage
-import os
 from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def db_teardown(code):
-    """declare a method to handle @app.teardown_appcontext
-    that calls storage.close()"""
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """ handle 404 error(not found)"""
-    return jsonify({"error": "Not found"}), 404
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')),
-            threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
