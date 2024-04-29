d98d11059e11d6a209392d2204da6da59305fb1d
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-04-30 02:22:42 +0300

    fixed pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Create flask app the blueprint 
+Create flask app the blueprint
 """
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
