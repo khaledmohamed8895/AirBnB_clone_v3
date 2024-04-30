26921e33ea2f3a9cd320a23be98253002248af46
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-04-30 22:54:41 +0300

    fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
 """
-Create flask app, app_views
+index
 """
+
 from flask import jsonify
 from api.v1.views import app_views
+
 from models import storage
 
 
-@app_views.route("/status")
-def api_status():
-    """ """
-    response = {"status": "OK"}
-    return jsonify(response)
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
 
 
-@app_views.route("/stats")
-def get_stats():
-    """ """
-    stats = {
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
@@ -26,4 +40,7 @@ def get_stats():
         "users": storage.count("User"),
     }
 
-    return jsonify(stats)
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
